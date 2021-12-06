756792c91ccffb5fff7fdcc51bc1044726a92553
ezzbreezn <bormix@inbox.ru>
2021-12-06 17:57:30 +0300

    ensembles_fix5

--- src/ensembles.py
+++ src/ensembles.py
@@ -50,16 +50,16 @@ class RandomForestMSE:
             self.algorithms.append(model)
             if return_train_loss is True:
                 train_pred = np.mean([m.predict(X) for m in self.algorithms], axis=0)
-                train_loss.append(((y - train_pred) ** 2).mean())
+                train_loss.append(np.sqrt(((y - train_pred) ** 2).mean()))
             if X_val is not None and y_val is not None:
                 val_pred = np.mean([m.predict(X_val) for m in self.algorithms], axis=0)
-                val_loss.append(((y_val - val_pred) ** 2).mean())
+                val_loss.append(np.sqrt(((y_val - val_pred) ** 2).mean()))
         if X_val is not None and y_val is not None and return_train_loss is True:
-            return np.sqrt(train_loss), np.sqrt(val_loss), np.cumsum(times)
+            return train_loss, val_loss, np.cumsum(times)
         elif X_val is not None and y_val is not None:
-            return np.sqrt(val_loss), np.cumsum(times)
+            return val_loss, np.cumsum(times)
         elif train_loss is True:
-            return np.sqrt(train_loss), np.cumsum(times)
+            return train_loss, np.cumsum(times)
 
 
     def predict(self, X):
@@ -96,8 +96,8 @@ class GradientBoostingMSE:
         self.feature_subsample_size = feature_subsample_size
         self.trees_parameters = trees_parameters
 
-    def RMSE(coef, y, pred, new_pred):
-        return np.sqrt(((y - pred - coef * new_pred) ** 2).mean())
+    def MSE(coef, y, pred, new_pred):
+        return ((y - pred - coef * new_pred) ** 2).mean()
 
 
     def fit(self, X, y, X_val=None, y_val=None, return_train_loss=False):
@@ -120,28 +120,31 @@ class GradientBoostingMSE:
             val_loss = []
             val_pred = np.zeros(X_val.shape[0])
 
+        def MSE(coef, y, pred, new_pred):
+            return ((y - pred - coef * new_pred) ** 2).mean()
+
         for i in range(self.n_estimators):
             model = DecisionTreeRegressor(max_depth=self.max_depth, max_features=self.feature_subsample_size, **self.trees_parameters)
             start = time.time()
-            model.fit(X, y - pred)
+            model.fit(X, 2 * (pred - y) / y.shape[0])
             end = time.time() - start
             times.append(end)
             new_pred = model.predict(X)
-            self.coef.append(minimize_scalar(RMSE, args=(y, pred, new_pred)).x)
+            self.coef.append(minimize_scalar(MSE, args=(y, pred, new_pred)).x)
             pred += self.learning_rate * self.coef[-1] * new_pred
             self.algorithms.append(model)
             if return_train_loss is True:
-                train_loss.append(((y - pred) ** 2).mean())
+                train_loss.append(np.sqrt(((y - pred) ** 2).mean()))
             if X_val is not None and y_val is not None:
                 new_val_pred = model.predict(X_val)
                 val_pred += self.learning_rate * self.coef[-1] * new_val_pred
-                val_loss.append(((y_val - val_pred) ** 2).mean())
+                val_loss.append(np.sqrt(((y_val - val_pred) ** 2).mean()))
         if X_val is not None and y_val is not None and return_train_loss is True:
-            return np.sqrt(train_loss), np.sqrt(val_loss), np.cumsum(times)
+            return train_loss, val_loss, np.cumsum(times)
         elif X_val is not None and y_val is not None:
-            return np.sqrt(val_loss), np.cumsum(times)
+            return val_loss, np.cumsum(times)
         elif return_train_loss is True:
-            return np.sqrt(train_loss), np.cumsum(times)
+            return train_loss, np.cumsum(times)
 
 
     def predict(self, X):
